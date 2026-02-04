218ddbe358930bc44ae6976c0acd110230a92a6a
taylor <tailorleaf@gmail.com>
2026-02-03 17:11:48 -0800

    simple get start

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include "json.hpp"
+#include <iostream>
+#include <curl/curl.h>
+
+size_t write_callback(char *ptr, size_t size, size_t nmemb,
+                      void *userdata);
+
+int main(){
+    return 0;
+}
